e2b721811463902e3928519f8de0793ebe77f22e
mysoyul <vega2k_@naver.com>
2023-11-10 09:46:42 +0900

    store 추가

---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import { createStore, createLogger } from "vuex"
+
+export const store = createStore({
+    plugins: process.env.NODE_ENV === 'development' ?
+        [createLogger()] : [],
+    state:{
+
+    },
+    mutations:{
+
+    },
+    getters:{
+
+    },
+    actions: {
+
+    }    
+})
