4ecd9711915c398377fc38e95f83665666bc10c9
mysoyul <vega2k_@naver.com>
2023-11-10 15:45:32 +0900

    environment files add

--- src/common/http-common.ts
+++ src/common/http-common.ts
@@ -1,7 +1,10 @@
 import axios, { AxiosInstance } from "axios"
 
+//http://localhost:4500/api
+const APIURL = process.env.VUE_APP_APIURL
+
 const apiClient: AxiosInstance = axios.create({
-    baseURL: "http://localhost:4500/api",
+    baseURL: APIURL,
     headers: {
         "Content-type": "application/json",
     },
